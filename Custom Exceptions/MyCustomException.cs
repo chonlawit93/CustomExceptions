c4c4ed69392d90d4a99706c69f6a97ba48f67291
chonlawitwith-pettersen <s311596@oslomet.no>
2022-09-08 18:26:56 +0200

    Done with tutorial

---
+++ Custom Exceptions/MyCustomException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CustomsExceptions.CustomExceptions
+{
+    public class MyCustomException : Exception
+    {
+        public MyCustomException()
+        {
+        }
+
+        public MyCustomException(string? message) : base(message)
+        {
+        }
+
+        public override string Message => "Custom error";
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CustomsExceptions.CustomExceptions;
+
+Console.WriteLine("Hello, World!");
+
+try
+{
+    TestException(true);
+}
+catch (MyCustomException ex)
+{
+    Console.WriteLine("Custom exception: " + ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Generic exception: " + ex.Message);
+}
+
+
+static void TestException(bool throwException)
+{
+    if (throwException)
+        throw new MyCustomException();
+
+    throw new IndexOutOfRangeException();
+} 
